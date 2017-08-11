a26132ab099bbc88ab97b7f9930c7a6db3149d44
Brady Brown <bobrown101@gmail.com>
2017-08-11 17:16:45 -0400

    started on piggy and switched to standard style from airbnb cause it was pissing me off

--- components/CheapOrFast.js
+++ components/CheapOrFast.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import GoeeyRadio from './GoeeyRadio';
+import AnimatedPiggy from './AnimatedPiggy';
 import RadioContainer from './RadioContainer';
 
 export default ({ preference, setPreferenceCheap, setPreferenceFast }) =>
   (
     <div>
+      <AnimatedPiggy />
       <RadioContainer
         height="100"
         width="100"
